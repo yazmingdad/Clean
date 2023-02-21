d5346f526dae13366b4805d904febcc1622b1638
yazmingdad <yazmingdad@gmail.com>
2023-02-22 00:52:14 +0100

    code first configuration

---
+++ Clean.Infrastructure/CleanDb/Entities/CleanDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Infrastructure.CleanDb.Entities
+{
+    public class CleanDbContext : DbContext
+    {
+        public CleanDbContext(DbContextOptions<CleanDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
